52986c778bcd94249fcb62f1f3eadff50d50fc9a
Derek <dcshiller@gmail.com>
2016-09-04 14:37:24 -0400

    add test

--- citehound.go
+++ citehound.go
@@ -15,6 +15,15 @@ func handler (w http.ResponseWriter, r *http.Request) {
 
 func main () {
   port := os.Getenv("PORT")
+
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+  console.log("HEY BUDDY< THIS IS A TEST< THE PORT NUMBER IS ")
+  console.log(port)
+
   http.HandleFunc("/", handler)
   http.ListenAndServe(port, nil)
   // http.ListenAndServe(":8080", nil)
