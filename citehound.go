7d83cdb7f69fff2b6b1424ec1e00b2b8ed014f7e
Derek <dcshiller@gmail.com>
2016-09-04 14:11:56 -0400

    Initialize Project

---
+++ citehound.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+  // "io/ioutil"
+  "net/http"
+  "fmt"
+)
+
+func handler (w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, World!!" )
+}
+
+func main () {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+}
