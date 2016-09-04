5afbc6969abace15d01dfc3ada00557608e63564
Derek <dcshiller@gmail.com>
2016-09-04 14:29:13 -0400

    try adding port variable

--- citehound.go
+++ citehound.go
@@ -4,13 +4,18 @@ import(
   // "io/ioutil"
   "net/http"
   "fmt"
+  "os"
 )
+
+
 
 func handler (w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, World!!" )
 }
 
 func main () {
+  port := os.Getenv("PORT")
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(port, nil)
+  // http.ListenAndServe(":8080", nil)
 }
