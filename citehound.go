c988adf34a8079a333f9b3bd8a85a6b2e3bb64c7
Derek <dcshiller@gmail.com>
2016-09-06 10:19:45 -0400

    change router static

--- citehound.go
+++ citehound.go
@@ -23,7 +23,7 @@ func main () {
   router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("index.tmpl.html")
-	router.Static("/static", "static")
+	router.Static("/assets", "assets")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
