c47936a678c1c854ff6aeb5af2b11a9289ea042b
Derek <dcshiller@gmail.com>
2016-09-06 09:21:56 -0400

    improve click and drag

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 var searchForm;
+var displayPanel;
 var boundMove
 
 function test () {
   console.log("passed");
 }
 
+function beginMove (DOMelement, e) {
+  var coords = {}
+  coords.clickOriginX = e.screenX;
+  coords.clickOriginY = e.screenY;
+  var rect = DOMelement.getBoundingClientRect();
+  coords.DOMOriginX = rect.left;
+  coords.DOMOriginY = rect.top;
+  boundMove =  move.bind(this, coords, DOMelement)
+  document.addEventListener("mousemove", boundMove)
+}
+
 function endMove () {
-  searchForm.removeEventListener("mousemove", boundMove)
+  document.removeEventListener("mousemove", boundMove)
 }
 
-function move (originx, originy, e) {
-  searchForm.style.left = e.screenX - originx + "px";
-  searchForm.style.top = e.screenY - originy + "px";
-}
-
-function beginMove (e) {
-  var originx = e.screenX;
-  var originy = e.screenY;
-  boundMove =  move.bind(this, originx, originy)
-   searchForm.addEventListener("mousemove", boundMove)
+function move (coords, DOMelement, e) {
+  DOMelement.style.left = coords.DOMOriginX + e.screenX - coords.clickOriginX + "px";
+  DOMelement.style.top = coords.DOMOriginY + e.screenY - coords.clickOriginY + "px";
 }
 
 function onLoad() {
   var searchButton = document.getElementById("searchButton")
   searchForm = document.getElementById("searchForm");
+  displayPanel = document.getElementById("displayPanel");
   var body = document.getElementsByTagName("body")[0];
-  searchForm.addEventListener("mousedown", beginMove)
-  body.addEventListener("mouseup", endMove)
+  searchForm.addEventListener("mousedown", beginMove.bind(this, searchForm))
+  displayPanel.addEventListener("mousedown", beginMove.bind(this, displayPanel))
+  document.addEventListener("mouseup", endMove)
 }
 
 
