744c921a94759cd33c3b48a10ce84912018e306e
Derek <dcshiller@gmail.com>
2016-09-07 14:51:51 -0400

    mess around

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 	router.Static("/javascript", "javascript")
 
