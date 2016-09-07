ddf6bfe2d151412936b7f6d30022b2f232a15bbc
Derek <dcshiller@gmail.com>
2016-09-07 14:55:36 -0400

    yo

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	// router.Static("/static", "static")
-	// router.Static("/javascript", "javascript")
-
-	router.GET("/ping", func(c *gin.Context) {
-
-		})
-
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.String(200, "pong")
-	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	// })
-
-	// router.Run(":" + port)
+	// port := os.Getenv("PORT")
+	//
+	// if port == "" {
+	// 	log.Fatal("$PORT must be set")
+	// }
+	//
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// // router.LoadHTMLGlob("templates/*.tmpl.html")
+	// // router.Static("/static", "static")
+	// // router.Static("/javascript", "javascript")
+	//
+	// router.GET("/ping", func(c *gin.Context) {
+	//
+	// 	})
+	//
+	// // router.GET("/", func(c *gin.Context) {
+	// // 	c.String(200, "pong")
+	// // 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// // })
+	//
+	// // router.Run(":" + port)
 }
