45eb273516254b9e75129fcbc1023a1294cb4d8a
Derek <dcshiller@gmail.com>
2016-09-09 18:17:36 -0400

    do something

--- citehound.go
+++ citehound.go
@@ -15,7 +15,14 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
   // fs := http.FileServer(http.Dir("Static"))
   // http.Handle("/",fs)
+  port := os.Getenv("PORT")
+  if (port == "") {port = "8080"}
+
+  if port == "" {
+    log.Fatal("$PORT must be set")
+  }
+
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
   http.HandleFunc("/", viewHandler)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":" + port, nil)
 }
