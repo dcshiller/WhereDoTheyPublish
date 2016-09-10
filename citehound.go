e30ce73f41400fbffd3de66e27bab32ec753bc75
Derek <dcshiller@gmail.com>
2016-09-10 12:31:49 -0400

    add parsed publication fields

--- citehound.go
+++ citehound.go
@@ -2,26 +2,50 @@ package main
 
 import (
     // "fmt"
-    // "io/ioutil"
+    "io/ioutil"
     "html/template"
     "net/http"
     "os"
     "log"
+    "bytes"
+    "encoding/json"
 )
 
-// type TestStruct struct {
-//   Name string
-// }
+// Your API key is YUwLUPslNpcO9nkK and your user ID is 7192.
+
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   t, _ := template.ParseFiles("index.tmpl.html")
-  // myTest := &TestStruct{Name: "yoyo"}
   t.Execute(w, nil)
 }
 
+func processWebData (url string) [][]byte {
+  resp, _ := http.Get(url)
+  byteStr, _ := ioutil.ReadAll(resp.Body)
+  bytesArr := bytes.Split(byteStr, []byte{'\n'})
+  resp.Body.Close()
+  return bytesArr
+}
+
+type publication struct {
+  Title string
+  Author string
+  Journal string
+}
+
+func jsonHandler (w http.ResponseWriter, r *http.Request) () {
+  Data := publication{Title: "Hidden Qualia", Author: "Derek Shiller", Journal: "Review of Philosophy and Psychology"}
+  jData, _ := json.Marshal(Data)
+  // fmt.Println(jData)
+  w.Header().Set("Content-Type","application/json")
+  // json.NewEncoder(w).Encode(Data)
+  w.Write(jData)
+}
+
 func main() {
-  // fs := http.FileServer(http.Dir("Static"))
-  // http.Handle("/",fs)
+  // bytesArr := processWebData("http://www.derekshiller.com")
+  // fmt.Println("HTML:\n\n", string(bytesArr[3]))
+
   port := os.Getenv("PORT")
   if (port == "") {port = "8080"}
 
@@ -31,5 +55,6 @@ func main() {
 
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
   http.HandleFunc("/", viewHandler)
+  http.HandleFunc("/json/",jsonHandler)
   http.ListenAndServe(":" + port, nil)
 }
