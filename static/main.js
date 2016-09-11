5128b3a58243b5d3cfbcffae228eea0680f29d70
Derek <dcshiller@gmail.com>
2016-09-11 16:25:07 -0400

    add params to ajax

--- static/main.js
+++ static/main.js
@@ -28,32 +28,49 @@ function move (coords, DOMelement, e) {
 
 function queryRequest (e) {
   e.preventDefault();
+  document.getElementById("intro").style.display = 'none';
   let xhttp = new XMLHttpRequest();
+  // let params = "test"
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
+    const pubList = document.getElementById("pubList")
+    pubList.innerHTML = ""
     let message = JSON.parse(this.responseText)
-    debugger
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < message.length; i++) {
       var newEl = document.createElement("li")
-      newEl.innerText = message[i].Journal
-      document.getElementById("pubList").appendChild(newEl)
+      newEl.innerText = message[i].Title + " " + message[i].Count
+      pubList.appendChild(newEl)
     }
  }
   }
   // let url = window.location.hostname == "localhost" ? "http://localhost:8080/json/" :
-  xhttp.open("GET","/json/", true)
-  xhttp.send();
+  xhttp.open("POST","/json/", true)
+  // var Data = new FormData(searchForm)
+  let fields = document.getElementsByClassName('authorField')
+  params = ""
+  for (let i = 0; i < fields.length ; i++) {
+    if (fields[i].value != ""){
+     params += (fields[i].value) + "|";
+  }}
+  params = params.slice(0, -1);
+  xhttp.send(params);
+}
+
+function showAbout() {
+  document.getElementById("intro").style.display = 'block';
 }
 
 function onLoad() {
   var searchButton = document.getElementById("searchButton")
+  var aboutButton = document.getElementById("aboutButton")
+  var body = document.getElementsByTagName("body")[0];
   searchForm = document.getElementById("searchForm");
   displayPanel = document.getElementById("displayPanel");
-  var body = document.getElementsByTagName("body")[0];
   searchForm.addEventListener("mousedown", beginMove.bind(this, searchForm))
   displayPanel.addEventListener("mousedown", beginMove.bind(this, displayPanel))
   document.addEventListener("mouseup", endMove)
   searchButton.addEventListener("click", queryRequest)
+  aboutButton.addEventListener("click", showAbout)
 }
 
 
