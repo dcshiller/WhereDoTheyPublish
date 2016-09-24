7807de7b8b69a1818a3d2b99bb6fd04c83d647b3
Derek <dcshiller@gmail.com>
2016-09-23 20:09:13 -0400

    better center text

--- static/main.js
+++ static/main.js
@@ -22,7 +22,7 @@ function addSample (sampleNum, e) {
         break;
     }
 
-    for (let i = 0; i <= 8; i++) {
+    for (let i = 0; i < 8; i++) {
       fields[i].value = sample[i]
     }
 }
@@ -33,44 +33,35 @@ function assignJournalLis () {
   pubList.innerHTML = ""
   let message = JSON.parse(this.responseText)
   for (let i = -1; i < message.length; i++) {
-    var newEl = document.createElement("li")
     if (i == -1) {
       if (message[1].Count == 0) {
-        newEl.innerHTML = `<span id="noResults"> No results found. </span>`
+        pubList.innerHTML = `<span id="noResults" class="middle"> No results found. </span>`
       } else {
         newEl.innerHTML = `<strong> Count </strong> <center> Journal Title </center> `
       }
-    } else if (message[i].Count > 0) {
-      // newEl.innerHTML = `<strong>${message[i].Count}</strong> <span>${message[i].Title}`
+    } else if (message[i].Count > 0 && message[i].Title != "") {
+      var newEl = document.createElement("li")
       newEl.innerHTML = `<strong>${message[i].Count}</strong> <span>${message[i].Title}</span>`
-      // pubList.appendChild(newEl)
+      pubList.appendChild(newEl)
     }
-    pubList.appendChild(newEl)
   }
   document.getElementById("whereTheyPublishButton").disabled = false;
   // document.getElementById("whereTheyCiteButton").disabled = false;
 }
 }
 
-function beginMove (DOMelement, e) {
-  var coords = {}
-  coords.clickOriginX = e.screenX;
-  coords.clickOriginY = e.screenY;
-  var rect = DOMelement.getBoundingClientRect();
-  coords.DOMOriginX = rect.left;
-  coords.DOMOriginY = rect.top;
-  boundMove =  move.bind(this, coords, DOMelement)
-  document.addEventListener("mousemove", boundMove)
-}
-
-function endMove () {
-  document.removeEventListener("mousemove", boundMove)
-}
-
-function getRanking (buttonId, url, e) {
-  e.preventDefault();
-  document.getElementById(buttonId).disabled = true;
+function enterWaitingState () {
+  document.getElementById("whereTheyPublishButton").disabled = true;
   document.getElementById("intro").style.display = 'none';
+  let pubList = document.getElementById("pubList")
+  pubList.innerHTML = "<span class='middle'> This could take a while</span>"
+
+}
+
+function getRanking (url, e) {
+
+  e.preventDefault();
+  enterWaitingState();
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = assignJournalLis;
   xhttp.open("POST",url, true)
@@ -81,22 +72,14 @@ function getRanking (buttonId, url, e) {
      authors += (fields[i].value) + "|";
   }}
   authors = authors.slice(0, -1);
-  filterVal = document.getElementById('filterSelector').value
+  filterVal = document.getElementsByClassName('selected')[0].innerText
+  console.log(filterVal)
   params = JSON.stringify({authors: authors, filter: filterVal})
   xhttp.send(params);
 }
 
 function getWhereTheyPublish (e) {
-    getRanking("whereTheyPublishButton", "/wheredotheypublish/", e)
-}
-
-// function getWhereTheyCite (e) {
-//     getRanking("whereTheyCiteButton", "/wheredotheycite/", e)
-// }
-
-function move (coords, DOMelement, e) {
-  DOMelement.style.left = coords.DOMOriginX + e.screenX - coords.clickOriginX + "px";
-  DOMelement.style.top = coords.DOMOriginY + e.screenY - coords.clickOriginY + "px";
+    getRanking( "/wheredotheypublish/", e)
 }
 
 function showAbout(e) {
@@ -115,6 +98,23 @@ function writeAuthorName (e){
    }
 }
 
+function selectLi (e) {
+  e.preventDefault();
+  let thisLi = e.target;
+  if (thisLi.className.includes("shown")) {e.stopPropagation()}
+  let selectables = Array.prototype.slice.call(document.getElementsByClassName("selectable"), 0)
+  selectables.forEach (function(el){el.className = "selectable";})
+  thisLi.className = thisLi.className + " selected"
+  setTimeout(function(){document.getElementById("filterUl").addEventListener("click", showSelectables)}, 1000)
+}
+
+function showSelectables (e) {
+  e.preventDefault();
+  let selectables = Array.prototype.slice.call(document.getElementsByClassName("selectable"), 0)
+  selectables.forEach (function(el){el.className += " shown";})
+  document.getElementById("filterUl").removeEventListener("click", showSelectables)
+}
+
 function onLoad() {
   var whereTheyPublishButton = document.getElementById("whereTheyPublishButton")
   // var whereTheyCiteButton = document.getElementById("whereTheyCiteButton")
@@ -125,15 +125,19 @@ function onLoad() {
   document.getElementById('clearButton').addEventListener("click", addSample.bind(this,3))
   searchForm = document.getElementById("searchForm");
   displayPanel = document.getElementById("displayPanel");
-  searchForm.addEventListener("mousedown", beginMove.bind(this, searchForm))
-  displayPanel.addEventListener("mousedown", beginMove.bind(this, displayPanel))
-  document.addEventListener("mouseup", endMove)
+  // searchForm.addEventListener("mousedown", beginMove.bind(this, searchForm))
+  // displayPanel.addEventListener("mousedown", beginMove.bind(this, displayPanel))
+  // document.addEventListener("mouseup", endMove)
   whereTheyPublishButton.addEventListener("click", getWhereTheyPublish)
   // whereTheyCiteButton.addEventListener("click", getWhereTheyCite)
   // whereTheyCiteButton.addEventListener("mouseover", showWarning)
   // whereTheyCiteButton.addEventListener("mouseleave", hideWarning)
   aboutButton.addEventListener("click", showAbout)
   document.getElementById("authorFieldOne").addEventListener("input", writeAuthorName)
+  let selectables = Array.prototype.slice.call(document.getElementsByClassName("selectable"), 0)
+  selectables.forEach(function(el){el.addEventListener("click", selectLi)})
+  document.getElementById("filterUl").addEventListener("click", showSelectables)
+  // document.getElementById("filterUl").addEventListener("mouseleave", hideSelectables)
 }
 
 
