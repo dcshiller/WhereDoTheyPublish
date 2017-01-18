31c9b58f9558386f8318dcd4bbcccb008fc2bab2
dcshiller <dcshiller@gmail.com>
2017-01-17 20:46:25 -0500

    restructure data view

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -8,14 +8,15 @@ class JournalsController < ApplicationController
   def show
     @journal = Journal.find(params[:id])
     @publication_count = @journal.publications.count
-    @years = @journal.publications.order(publication_year: :desc).group(:publication_year).paginate(page: params[:page], per_page: 10)
+    @years = @journal.publications.order(publication_year: :desc).group(:publication_year).paginate(page: params[:page], per_page: 100)
     @focused = "Data"
     @focused_datatype = "Journals"
   end
 
   def year
+    @year = params[:year]
     @journal = Journal.find(params[:journal_id])
-    @publications = @journal.publications.where(publication_year: params[:year]).paginate(page: params[:page], per_page: 10)
+    @publications = @journal.publications.where(publication_year: @year).paginate(page: params[:page], per_page: 10)
     @focused = "Data"
     @focused_datatype = "Journals"
   end
