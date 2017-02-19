f6b394acce292c0d20ef7c163b5fcbbf3a1faaa6
dcshiller <dcshiller@gmail.com>
2017-02-19 10:39:51 -0500

    add spacing to cleaner

--- app/services/author_cleaner.rb
+++ app/services/author_cleaner.rb
@@ -14,4 +14,54 @@ class AuthorCleaner
   def self.delete_with_no_pubs!
     Author.where.not(id: Author.distinct.joins(:publications).pluck(:id)).destroy_all
   end
+
+  def self.space_initials!
+    authors = Author.where("first_name LIKE '_._.' OR first_name LIKE '_._._.'")
+    authors.each do |author|
+      first_name = author.first_name
+      new_first_name = first_name.gsub(".",". ").gsub("  "," ").strip
+      dup_author = Author.find_by(first_name: new_first_name, middle_initial: author.middle_initial, last_name: author.last_name)
+      if dup_author
+        author.merge_into(dup_author)
+      else
+        print first_name + " => "
+        author.first_name = new_first_name
+        puts new_first_name
+        author.save
+        puts author.name
+      end
+    end
+
+    authors = Author.where("middle_initial LIKE '_._.' OR middle_initial LIKE '_._._.'")
+    authors.each do |author|
+      middle_initial = author.middle_initial
+      new_middle_initial = middle_initial.gsub(".",". ").gsub("  "," ").strip
+      dup_author = Author.find_by(first_name: author.first_name, middle_initial: new_middle_initial, last_name: author.last_name)
+      if dup_author
+        author.merge_into(dup_author)
+      else
+        print middle_initial + " => "
+        author.middle_initial = new_middle_initial
+        puts new_middle_initial
+        author.save
+        puts author.name
+      end
+    end
+  end
+
+  def self.move_initials_to_middle!
+    authors = Author.where("first_name LIKE '% %'")
+    authors.each do |author|
+      first_name = author.first_name
+      middle_initial = author.middle_initial
+      new_first_name, additional_initials = first_name.split(" ")[0], first_name.split(" ")[1..-1].join(" ")
+      new_middle_initials = middle_initial.blank? ? additional_initials : (additional_initials .sp middle_initial)
+      dup_author = Author.find_by(first_name: new_first_name, middle_initial: new_middle_initials, last_name: author.last_name)
+      if dup_author
+        author.merge_into dup_author
+      else
+        author.update_attributes(first_name: new_first_name, middle_initial: new_middle_initials)
+      end
+    end
+  end
 end
--- test/services/author_cleaner_test.rb
+++ test/services/author_cleaner_test.rb
@@ -24,4 +24,27 @@ class AuthorCleanerTest < ActiveSupport::TestCase
     AuthorCleaner.delete_with_no_pubs!
     there_must_be 1, Author
   end
+
+  test "spreads middle initials" do
+    create :author, first_name: "D.C.", last_name: "Shiller"
+    create :author, middle_initial: "M.B.N."
+    create :author, first_name: "W.V.O.", middle_initial: nil, last_name: "Quine"
+    create :author, first_name: "W. V. O.", middle_initial: nil, last_name: "Quine"
+    AuthorCleaner.space_initials!
+    assert_equal "D. C.", Author.first.first_name
+    assert_equal "M. B. N.", Author.second.middle_initial
+    there_must_be 3, Author
+  end
+
+  test "moves middle initials correctly" do
+    create :author, first_name: "Achille C.", middle_initial: nil, last_name: "Varzi"
+    create :author, first_name: "D. B.", middle_initial: "", last_name: "Cooper"
+    create :author, first_name: "Alfred J.", middle_initial: "", last_name: "Ayer"
+    create :author, first_name: "Alfred", middle_initial: "J.", last_name: "Ayer"
+    AuthorCleaner.move_initials_to_middle!
+    assert_equal "C.", Author.first.middle_initial
+    assert_equal "Achille", Author.first.first_name
+    assert_equal "B.", Author.second.middle_initial
+    there_must_be 3, Author
+  end
 end
