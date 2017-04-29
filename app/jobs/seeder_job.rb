3a3bd685f73117649bdc8a99157f8117304843af
dcshiller <dcshiller@gmail.com>
2017-04-28 21:24:30 -0400

    update seederjob

--- app/jobs/seeder_job.rb
+++ app/jobs/seeder_job.rb
@@ -17,12 +17,18 @@ class SeederJob
                   ps: "Philosophy of Science"
                 }
 
-  def self.perform
-    initial_settings
-    set_terms
-    set_journals
+  def self.enqueue_cats
+    CATEGORIES.each do |k, _|
+      Resque.enqueue(SeederJob,k)
+    end
   end
-  
+
+  def self.perform(step)
+    initial_settings if step == 1
+    set_journals if step == 2
+    set_terms(step) if step.is_a? Symbol
+  end
+
   def self.initial_settings
     average = CATEGORIES.keys.map { |k| [k, 5] }.to_h
     Journal.update_all(categorization: average)
@@ -30,12 +36,13 @@ class SeederJob
     Publication.update_all(categorization: average)
   end
 
-  def self.set_terms
+  def self.set_terms(cat)
+    cat = cat.to_sym
     terms = {
               ap: %w[Plato Epicurus Akrasia Eleatic Eudaemonia Aristotle Stoic Socrates Lucretius Presocratic Greek Roman Augustine Boethius Classical],
               as: %w[Confucious Dao Nayaya Chinese Indian Buddhis Eastern],
               ae: %w[Corporation Love Obligation Trial Friend Queer Pornography Sex Euthanasia Abortion Doctor Patient Drug Death Life Health Disease Consent Market Libertarianism Exchange Capitalism Communism Price Regulation Sale Punishment],
-              em: %w[Epistemolo Argument Externalism Internalism Epistemic Evidence Belief Credence Knowledge Empiricism Foundationalism Coherentism Foundationalism Probability Likelihood Parsimony Perception Memory Sensitivity Gettier Skeptic Bayesianism ],
+              em: %w[Epistemolo Externalism Internalism Epistemic Evidence Belief Credence Knowledge Empiricism Foundationalism Coherentism Probability Likelihood Parsimony Perception Memory Sensitivity Gettier Skeptic Bayesianism ],
               et: %w[Normative Motivation Egoism Wrongs Happiness Akratic Imperative Rawls Guilt Ought Killing Sidgwick Parfit Singer Externalism Internalism Love Friend Reason Akrasia Fair Evil Blame Life Deontic Moral Ethic Desert Consequentialism Value Rights Justice Good Wellbeing Punishment Obligation Obligatory Virtue Charity Utilitarianism Deontology],
               lo: %w[Axiom Computability Completeness Necessity Truth Logic S5 Modal S4],
               mp: %w[Descartes Hume Nietzsche Kierkegaard Hegel Modern Cartesian Bayle Aquinas Scholastic Thomastic Locke Spinoza Kant Wolff Leibniz Reid Occasionalism Transcedentalism],
@@ -47,15 +54,13 @@ class SeederJob
               pr: %w[God Thomastic Religious Mystic Thomism Aquinas Church Devout Religion Theo Faith Muslim Christian Heaven Hell Prayer Evil Divine Divinity Salvation Omniscience Omnipotence],
               ps: %w[Cell Cognitive Correlation Scientist Physics Relativity Atom Subatomic Animal Brain Neuron Science Genome Causation Physics Biology Evolution Quantum Chemistry]
             }
-    terms.each do |category, cat_terms|
-      cat_terms.each do |term|
-        Publication.where("title LIKE '%#{term}%'").each do |pub|
-          cats = pub.categorization
-          cats[category] = 100
-          pub.categorization = cats
-          pub.save
-          print "."
-        end
+    terms[cat].each do |term|
+      Publication.where("title LIKE '%#{term}%'").each do |pub|
+        cats = pub.categorization
+        cats[cat] = 100
+        pub.categorization = cats
+        pub.save
+        print "."
       end
     end
   end
@@ -65,7 +70,7 @@ class SeederJob
                   ap: ["Apeiron"],
                   as: ["Asian Philosophy", "Dao", "Journal of Chinese Philosophy"],
                   ae: ["Science and Engineering Ethics", "Neuroethics", "Educational Philosophy and Theory", "Environmental Ethics", "Journal of Law, Medicine, and Ethics", "Cambridge Quarterly of Healthcare Ethics", "Developing World Bioethics", "Bioethics", "American Journal of Bioethics", "Medicine and Philosophy", "Business Ethics Quarterly", "Journal of Business Ethics"],
-                  em: ["Episteme%", "Erkenntnis", "Monist", "Synthese", "Analysis","Thought", "Dialectica", "Journal of Philosophy", "Philosophical Review", "American Philosophical Quarterly", "Analytic Philosophy", "Review of Metaphysics", "Theory and Decision"],
+                  em: ["Episteme%", "Erkenntnis", "Monist", "Synthese", "Analysis","Thought", "Dialectica", "Journal of Philosophy", "Philosophical Review", "American Philosophical Quarterly", "Analytic Philosophy", "Theory and Decision"],
                   et: ["%Ethics%", "Environmental Ethics", "Bioethics", "Journal of Philosophy", "Ethical Theory and Moral Practice", "Utiliats", "Ethics", "Journal of Ethics", "Journal of Ethics and Social Philosophy", "Theory and Decision"],
                   lo: ["Journal of Symbolic Logic", "Journal of Philosophical Logic", "Bulletin of Symbolic Logic"],
                   mp: ["Hume Studies", "British Journal for the History of Philosophy", "Journal of the History of Philosophy",  "New Nietzsche Studies", "Bulletin of the Hegel Society of Great Britain"],
@@ -73,7 +78,7 @@ class SeederJob
                   pa: ["British Journal of Aesthetics", "Journal of Aesthetics and Art Criticism"],
                   pl: ["Linguistics and Philosophy", "Semiotica","Monist", "Synthese", "Analysis", "Thought", "Dialectica", "Journal of Philosophy", "Philosophical Review", "American Philosophical Quarterly", "Analytic Philosophy", "Review of Metaphysics", "%Language"],
                   po: ["Journal of Law, Medicine, and Ethics", "Political Theory", "Law and Philosophy", "Journal of Ethics and Social Philosophy"],
-                  pm: ["Phenomenology and the Cognitive Sciences", "Neuroethics", "Consciousness and Cognition", "Behavioral and Brain Studies", "Mind%", "Journal of Consciousness Studies"],
+                  pm: ["Phenomenology and the Cognitive Sciences", "Neuroethics", "Consciousness and Cognition", "Behavioral and Brain Studies", "Mind%", "Journal of Consciousness Studies", "Review of Metaphysics"],
                   pr: ["Faith and Philosophy", "Religious Studies", "Ars Disputandi", "Augustinian Studies", "American Catholic Philosophical Quarterly"],
                   ps: ["Science and Engineering Ethics", "Hyle", "Journal for General Philosophy of Science", "Biology and Philosophy", "Brititsh Journal for the Philosophy of Science"]
               }
