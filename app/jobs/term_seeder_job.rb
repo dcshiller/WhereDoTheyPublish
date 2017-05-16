2ab5ca38678fecf755e26e31b17942c45d6c5f6c
dcshiller <dcshiller@gmail.com>
2017-05-16 07:06:56 -0400

    expand seeder categorizations

--- app/jobs/term_seeder_job.rb
+++ app/jobs/term_seeder_job.rb
@@ -14,19 +14,19 @@ class TermSeederJob
 
   def self.set_terms(cat)
     terms = {
-              ap: %w[Plato Epicurus Akrasia Eleatic Eudaemonia Aristotle Stoic Socrates Lucretius Presocratic Greek Roman Augustine Boethius Classical],
+              ap: %w[Plato Epicurus Akrasia Eleatic Eudaemonia Aristotle Stoic Socrates Lucretius Presocratic Greek Roman Augustine Boethius Classical Parmenides Empedocles],
               as: %w[Confucious Dao Nayaya Chinese Indian Buddhis Eastern],
               ae: %w[Corporation Love Obligation Trial Friend Queer Pornography Sex Euthanasia Abortion Doctor Patient Drug Death Life Health Disease Consent Market Libertarianism Exchange Capitalism Communism Price Regulation Sale Punishment],
-              em: %w[Epistemolo Externalism Internalism Epistemic Evidence Belief Credence Knowledge Empiricism Foundationalism Coherentism Probability Likelihood Parsimony Perception Memory Sensitivity Gettier Skeptic Bayesianism ],
-              et: %w[Normative Motivation Egoism Wrongs Happiness Akratic Imperative Rawls Guilt Ought Killing Sidgwick Parfit Singer Externalism Internalism Friend Reason Akrasia Fair Evil Blame Life Deontic Moral Ethic Desert Consequentialism Value Rights Justice Good Wellbeing Punishment Obligation Obligatory Virtue Charity Utilitarianism Deontology],
+              em: %w[ Ignorance Decision Disagreement Rationality Epistemolo Externalism Internalism Epistemic Evidence Belief Credence Knowledge Empiricism Foundationalism Coherentism Probability Likelihood Parsimony Perception Memory Sensitivity Gettier Skeptic Bayesianism ],
+              et: %w[ Distributive Normative Motivation Egoism Wrongs Happiness Akratic Imperative Rawls Guilt Ought Killing Sidgwick Parfit Singer Externalism Internalism Friend Reason Akrasia Fair Evil Blame Life Deontic Moral Ethic Desert Consequentialism Value Rights Justice Good Wellbeing Punishment Obligation Obligatory Virtue Charity Utilitarianism Deontology],
               lo: %w[Axiom Computability Completeness Necessity Truth Logic S5 Modal S4],
               mp: %w[Descartes Hume Nietzsche Kierkegaard Hegel Modern Cartesian Bayle Aquinas Scholastic Thomastic Locke Spinoza Kant Wolff Leibniz Reid Occasionalism Transcedentalism],
-              mt: %w[Platon Aristotelian Humean Cartesian Event Ability Irrealism Disposition Proposition World Sorites Paradox Reality Truthmaker Correspondence Realism Antirealism Nominalism Externalism Trope Mereological Supervaluation Color Modal Explanation Parthood Metaphysics Presentism Objects Vagueness Time Space Fictionalism Physicalism Dualism Metaphysics Modality Existence Essence Grounding Necessity Abstract Number Naturalism],
+              mt: %w[ Platon Aristotelian Humean Cartesian Event Ability Irrealism Disposition Proposition World Sorites Paradox Reality Truth Austere Correspondence Realism Antirealism Nominalism Externalism Trope Mereological Supervaluation Color Modal Explanation Parthood Metaphysics Presentism Objects Vagueness Time Space Fictionalism Physicalism Dualism Metaphysics Modality Existence Essence Grounding Necessity Abstract Number Naturalism],
               pa: %w[Aesthetics Art Film Poem Poetry Literature Beauty],
-              pl: %w[Word Truth Content Proposition Frege Russell Lexical Name Determinism Vagueness Intensional Semantics Pragmatics Phonetic Sense Refer Guise Meaning Predicate Negation Implicature Sign Compositional Define Definition Concept],
+              pl: %w[Word Truth Content Proposition Frege Russell Lexical Name Determinism Vagueness Intensional Semantics Pragmatics Phonetic Sense Refer Guise Meaning Predicate Negation Implicature Sign Compositional Define Definition Concept Predicat],
               po: %w[Privacy Autonomy Paternalism Authority Libertarianism Freedom Guilt Innocence Government Trial Jury Society Law Democracy Consent Totalitarian Justice Citizen Civic Communism Vote Civil Tax Constructivism],
-              pm: %w[Motivation Happiness Seeing Properties Property Chalmers Searle Intention Will Agent Agency Freedom Knowledge Belief Memory Love Sensation Russell Intention Imagination Physicalism Dualism Consciousness Intentionality Perception Phenomenology Phenomenal Qualia Brain Cognitiv Seeing Rational Reason Emotion Concept],
-              pr: %w[God Thomastic Religious Mystic Thomism Aquinas Church Devout Religion Theo Faith Muslim Christian Heaven Hell Prayer Evil Divine Divinity Salvation Omniscience Omnipotence],
+              pm: %w[ Preference Psycholog Motivation Happiness Seeing Properties Property Chalmers Searle Intention Will Agent Agency Freedom Knowledge Belief Memory Love Sensation Russell Intention Imagination Physicalism Dualism Consciousness Intentionality Perception Phenomenology Phenomenal Qualia Brain Cognitiv Seeing Rational Reason Emotion Concept],
+              pr: %w[ Teleo God Thomastic Religious Mystic Thomism Aquinas Church Devout Religion Theo Faith Muslim Christian Heaven Hell Prayer Evil Divine Divinity Salvation Omniscience Omnipotence],
               ps: %w[Cell Cognitive Correlation Scientist Physics Relativity Atom Subatomic Animal Brain Neuron Science Genome Causation Physics Biology Evolution Quantum Chemistry]
             }
     terms[cat].each do |term|
