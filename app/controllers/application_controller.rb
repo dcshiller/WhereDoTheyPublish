e6c9f433d43f879e54546551dfe46ae85d32eda5
dcshiller <dcshiller@gmail.com>
2017-06-25 21:11:28 -0400

    expand shown cats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   def sift_categories(categorizable)
     @categories = categorizable.cat.map { |k, v| [categorizable.category_name[k],v] if v > 0 }.compact
     @categories.sort! { |a, b| b[-1] <=> a[-1] }
-    @categories = @categories[0...3]
+    @categories.reject! { |a, b| b < 10 }
+    @categories = @categories[0...5]
   end
 end
