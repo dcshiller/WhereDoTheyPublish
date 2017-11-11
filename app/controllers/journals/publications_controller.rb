dd2c77d009d8ab4a854b67fc96c74a0105248dab
dcshiller <dcshiller@gmail.com>
2017-11-10 21:07:06 -0500

    cleanup controller

--- app/controllers/journals/publications_controller.rb
+++ app/controllers/journals/publications_controller.rb
@@ -3,17 +3,21 @@ class Journals::PublicationsController < ApplicationController
   before_action :find_journal, only: [:index]
 
   def index
-    @years = @journal.articles.order(publication_year: :desc).group(:publication_year).paginate(page: params[:page], per_page: 80)
+    @years = @journal.articles.order(publication_year: :desc).
+                      group(:publication_year).
+                      paginate(page: params[:page], per_page: 80)
   end
 
   def year
     @year = params[:year].to_i
     @journal = Journal.find(params[:journal_id])
-    @publications = @journal.articles.where(publication_year: @year).paginate(page: params[:page], per_page: 100).order(:volume, :number, :pages, :title)
+    @publications = @journal.articles.where(publication_year: @year).
+                             paginate(page: params[:page], per_page: 100).
+                            order(:volume, :number, :pages, :title)
   end
 
   private
-  
+
   def find_journal
     @journal = Journal.find(params[:journal_id])
   end
@@ -24,6 +28,7 @@ class Journals::PublicationsController < ApplicationController
   end
 
   def journal_params
-    params.require(:journal).permit(:name, :publication_start, :publication_end, :display_name)
+    params.require(:journal).
+           permit(:name, :publication_start, :publication_end, :display_name)
   end
 end
